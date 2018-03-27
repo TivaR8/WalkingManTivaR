0ed36ca447746dd724640c464b567bae27febbb8
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-03-27 14:20:57 -0400

    Initial commit

---
+++ WalkingManTivaR/WalkingManForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WalkingManTivaR
+{
+    partial class frmWalkingMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWalk = new System.Windows.Forms.Button();
+            this.picWalkMan = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picWalkMan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnWalk
+            // 
+            this.btnWalk.Location = new System.Drawing.Point(109, 17);
+            this.btnWalk.Name = "btnWalk";
+            this.btnWalk.Size = new System.Drawing.Size(75, 23);
+            this.btnWalk.TabIndex = 0;
+            this.btnWalk.Text = "Walk";
+            this.btnWalk.UseVisualStyleBackColor = true;
+            this.btnWalk.Click += new System.EventHandler(this.btnWalk_Click);
+            // 
+            // picWalkMan
+            // 
+            this.picWalkMan.Image = global::WalkingManTivaR.Properties.Resources.walk9;
+            this.picWalkMan.Location = new System.Drawing.Point(49, 46);
+            this.picWalkMan.Name = "picWalkMan";
+            this.picWalkMan.Size = new System.Drawing.Size(205, 221);
+            this.picWalkMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picWalkMan.TabIndex = 1;
+            this.picWalkMan.TabStop = false;
+            // 
+            // frmWalkingMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 298);
+            this.Controls.Add(this.picWalkMan);
+            this.Controls.Add(this.btnWalk);
+            this.Name = "frmWalkingMan";
+            this.Text = "Walking Man By Tiva Rait";
+            ((System.ComponentModel.ISupportInitialize)(this.picWalkMan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWalk;
+        private System.Windows.Forms.PictureBox picWalkMan;
+    }
+}
+
---
+++ WalkingManTivaR/WalkingManForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+/*
+ * Created by: Tiva Rait
+ * Created on: 27-03-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #20 - Walking Man 
+ * This program shows a man walking fast
+*/
+
+namespace WalkingManTivaR
+{
+    public partial class frmWalkingMan : Form
+    {
+        public frmWalkingMan()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWalk_Click(object sender, EventArgs e)
+        {
+            // Declaring constants and variables
+            const byte MAX_FRAMES = 10;
+            int pictureFrameCounter = 1;
+
+            // Continue through this loop until MAX_FRAMES is reached
+            while  (pictureFrameCounter < MAX_FRAMES + 1)
+            {
+                if (pictureFrameCounter == 1)
+                {
+                    this.picWalkMan.Image = Properties.Resources.walk1;
+                }
+                else if (pictureFrameCounter == 2)
+                {
+                    this.picWalkMan.Image = Properties.Resources.walk2;
+                }
+                else if (pictureFrameCounter == 3)
+                {
+                    this.picWalkMan.Image = Properties.Resources.walk3;
+                }
+                else if (pictureFrameCounter == 4)
+                {
+                    this.picWalkMan.Image = Properties.Resources.walk4;
+                }
+                else if (pictureFrameCounter == 5)
+                {
+                    this.picWalkMan.Image = Properties.Resources.walk5;
+                }
+                else if (pictureFrameCounter == 6)
+                {
+                    this.picWalkMan.Image = Properties.Resources.walk6;
+                }
+                else if (pictureFrameCounter == 7)
+                {
+                    this.picWalkMan.Image = Properties.Resources.walk7;
+                }
+                else if (pictureFrameCounter == 8)
+                {
+                    this.picWalkMan.Image = Properties.Resources.walk8;
+                }
+                else if (pictureFrameCounter == 9)
+                {
+                    this.picWalkMan.Image = Properties.Resources.walk9;
+                }
+                else if (pictureFrameCounter == 10)
+                {
+                    this.picWalkMan.Image = Properties.Resources.walk10;
+                }
+
+                // To Slow down the process
+                Thread.Sleep(150);
+                //Refresh the form
+                this.Refresh();
+                //Increment picture counter
+                pictureFrameCounter = pictureFrameCounter + 1;
+
+            }
+        }
+    }
+}
